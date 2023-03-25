3285d2d6599c8f303edd38af36431e4a4dd12f1a
shiuv <shiuvic@gmail.com>
2023-03-25 22:10:37 +0800

    update

--- robot_baseline/main.py
+++ robot_baseline/main.py
@@ -2,6 +2,7 @@ import gym
 import os
 
 import numpy
+import numpy as np
 from stable_baselines3 import PPO
 import torch as th
 import torch.nn as nn
@@ -10,10 +11,12 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.type_aliases import TensorDict
 from robot_baseline import robot
 from stable_baselines3.ppo.policies import CnnPolicy
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 # Parallel environments
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.preprocessing import get_flattened_obs_dim, is_image_space
+
 
 class CustomCNN(BaseFeaturesExtractor):
     """
@@ -30,70 +33,58 @@ class CustomCNN(BaseFeaturesExtractor):
 
         total_concat_size = 0
         for key, subspace in observation_space.spaces.items():
-            # print(key, subspace)
-            if is_image_space(subspace):
-                # extractors[key] = NatureCNN(subspace, features_dim=cnn_output_dim)
-                total_concat_size += cnn_output_dim
+            print(key, subspace)
+            if key != 'torque':
+                print(subspace.shape)
+                extractors[key] = nn.Sequential(nn.MaxPool1d(3),
+                                                nn.MaxPool1d(2)
+                                                )
+                total_concat_size += 21
             else:
-                # The observation key is a vector, flatten it if needed
-                # if key != 'torque':
-                #     extractors[key] = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=0),
-                #                                     nn.ReLU())
+                extractors[key] = nn.Sequential(nn.Flatten(),
+                                                nn.Linear(4, 16),
+                                                nn.ReLU()
+                                                )
+                total_concat_size += 16
+
+                # if key == 'accx':
+                #     extractors[key] = nn.Sequential(
+                #                                     nn.LazyLinear(64),
+                #                                     nn.ReLU(),
+                #                                     nn.Linear(64, 16),
+                #                                     nn.ReLU(),
+                #                                     # nn.ReLU()
+                #                                     )
+                #     total_concat_size += 16
                 #
+                # elif key == 'accy':
+                #     extractors[key] = nn.Sequential(
+                #                                     nn.LazyLinear(64),
+                #                                     nn.ReLU(),
+                #                                     nn.Linear(64, 32),
+                #                                     nn.ReLU(),
+                #                                     nn.Linear(32, 8),
+                #                                     nn.ReLU()
+                #                                     )
+                #     total_concat_size += 8
                 #
-                #     # extractors[key] = nn.Sequential(nn.Flatten(),
-                #     #                                 nn.Linear(128, 64),
-                #     #                                 nn.ReLU(),
-                #     #                                 nn.Linear(64, 32))
-                #     total_concat_size += 1*381
-                # else:
-                #     # extractors[key] = nn.Sequential(nn.Flatten(),
-                #     #                                 nn.Linear(4, 16),
-                #     #                                 nn.ReLU(),
-                #     #                                 nn.Linear(16, 64),
-                #     #                                 nn.Linear(64, 32))
-                #     extractors[key] = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=2, kernel_size=2, padding=0),
-                #                                     nn.ReLU())
-                #     total_concat_size += 4*64
-
-                if key == 'accx':
-                    extractors[key] = nn.Sequential(nn.Flatten(),
-                                                    nn.LazyLinear(64),
-                                                    nn.ReLU(),
-                                                    nn.Linear(64, 16),
-                                                    nn.ReLU(),
-                                                    # nn.ReLU()
-                                                    )
-                    total_concat_size += 16
-
-                elif key == 'accy':
-                    extractors[key] = nn.Sequential(nn.Flatten(),
-                                                    nn.LazyLinear(64),
-                                                    nn.ReLU(),
-                                                    nn.Linear(64, 32),
-                                                    nn.ReLU(),
-                                                    nn.Linear(32, 8),
-                                                    nn.ReLU()
-                                                    )
-                    total_concat_size += 8
-
-                elif key == 'accz':
-                    extractors[key] = nn.Sequential(nn.Flatten(),
-                                                    nn.Linear(128, 64),
-                                                    nn.ReLU(),
-                                                    nn.Linear(64, 8),
-                                                    nn.ReLU()
-                                                    )
-                    total_concat_size += 8
-
-                elif key == 'torque':
-                    extractors[key] = nn.Sequential(nn.Flatten(),
-                                                    nn.LazyLinear(16),
-                                                    nn.ReLU(),
-                                                    nn.Linear(16, 8),
-                                                    nn.ReLU()
-                                                    )
-                    total_concat_size += 8
+                # elif key == 'accz':
+                #     extractors[key] = nn.Sequential(
+                #                                     nn.Linear(128, 64),
+                #                                     nn.ReLU(),
+                #                                     nn.Linear(64, 8),
+                #                                     nn.ReLU()
+                #                                     )
+                #     total_concat_size += 8
+                #
+                # elif key == 'torque':
+                #     extractors[key] = nn.Sequential(
+                #                                     nn.LazyLinear(16),
+                #                                     nn.ReLU(),
+                #                                     nn.Linear(16, 8),
+                #                                     nn.ReLU()
+                #                                     )
+                #     total_concat_size += 8
 
         self.flattened_tensor = nn.Flatten()
 
@@ -106,50 +97,18 @@ class CustomCNN(BaseFeaturesExtractor):
         encoded_tensor_list = []
 
         for key, extractor in self.extractors.items():
+
             encoded_tensor_list.append(extractor(observations[key]))
+            print('name', {key}, '->', np.shape(extractor(observations[key])))
+            # print(np.shape(extractor(observations[key])))
         # print(encoded_tensor_list)
 
         return th.cat(encoded_tensor_list, dim=1)
-
-
-class CustomCombinedExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Dict):
-
-        super(CustomCombinedExtractor, self).__init__(observation_space, features_dim=1)
-
-        extractors = {}
-
-        total_concat_size = 0
-        print('hello world')
-        for key, subspace in observation_space.spaces.items():
-            if is_image_space(subspace):
-
-                extractors[key] = nn.Sequential(nn.MaxPool2d(4), nn.Flatten())
-                total_concat_size += subspace.shape[1] // 4 * subspace.shape[2] // 4
-            else:
-                # Run through a simple MLP
-                extractors[key] = nn.Linear(subspace.shape[0], 16)
-                total_concat_size += 16
-
-        self.extractors = nn.ModuleDict(extractors)
-
-        # Update the features dim manually
-        self._features_dim = total_concat_size
-
-    def forward(self,observations: TensorDict) -> th.Tensor:
-        encoded_tensor_list = []
-
-        for key, extractor in self.extractors.items():
-            encoded_tensor_list.append(extractor(observations[key]))
-        # print(encoded_tensor_list)
-        return th.cat(encoded_tensor_list, dim=1)
-
 
 
 policy_kwargs = dict(
     features_extractor_class=CustomCNN
 )
-
 checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/', name_prefix='rl_model')
 
 ###########################################PPO##############################################
@@ -189,8 +148,8 @@ env = make_vec_env('robot-v0', n_envs=1)
 
 model = SAC("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
 model.learn(total_timesteps=300000, log_interval=4, callback=checkpoint_callback)
-# model.save("sac_robot")
-# model = env.close()
+model.save("sac_robot")
+model = env.close()
 
 # del model # remove to demonstrate saving and loading
 #
